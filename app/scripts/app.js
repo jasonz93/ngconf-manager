610f56275deceb92da8833920b4067dc1392e47d
zhangsihao <zhangsihao@58ganji.com>
2017-03-15 18:50:21 +0800

    Init

---
+++ app/scripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ngconfManagerApp
+ * @description
+ * # ngconfManagerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngconfManagerApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngMaterial'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+  .controller('MenuCtrl', function () {
+      
+  })
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngconfManagerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngconfManagerApp
+ */
+angular.module('ngconfManagerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
